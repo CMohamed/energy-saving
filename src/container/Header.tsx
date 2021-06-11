ea872d1f223ee8c67d77fcdf9b165fe44a489e65
Abdelghani HOUBBADI <houbbadi.abdelghani@gmail.com>
2021-06-11 13:44:20 +0100

    draft 2

--- src/container/Header.tsx
+++ src/container/Header.tsx
@@ -1,4 +1,6 @@
-import Button from './Button'
+import {Button} from '@material-ui/core';
+import Typography from '@material-ui/core/Typography';
+
 
 const Header = () => {
     const onClick=()=>{
@@ -6,19 +8,30 @@ const Header = () => {
     }
 
     return (
-        <div  className='header'>
-            <h1>Sign in</h1>
-            <h2>Sign in on the internal platform</h2>
-            <Button color='blue' text='Login with Facebook' onClick={onClick}/>
-            <Button color='blue' text='Login with Google' onClick={onClick}/>
+        <form  className='container'>
+            <div className='css-1gm9hch'>
+                <h2  className='MuiTypography-root MuiTypography-h2 css-zoocp0-MuiTypography-root'>Sign in</h2>
+                <p>Sign in on the internal platform</p>
+            </div>
+            <div className='MuiGrid-root MuiGrid-container MuiGrid-spacing-xs-3 css-h51dfx-MuiGrid-root'>
+                <Button variant="contained" color="primary" onClick={onClick}>Login with Facebook</Button>
+                <Button variant="contained" color="primary" onClick={onClick}>Login with google</Button>
+            </div>
             <h3>or login with email address</h3>
+            <div>
             <label><b>Email Adress</b></label>
             <input type="text" placeholder="Enter Username" name="uname" required></input>
+            </div>
+            <div>
             <label><b>Password</b></label>
             <input type="password" placeholder="Enter Password" name="psw" required></input>
-            <Button color='blue' text='Sign in now' onClick={onClick}/>
-            <h3>Don't have an account? Sign up</h3>
-        </div>
+            </div>
+            <div>
+            <Button variant="contained" color="primary" onClick={onClick}>Sign in now</Button>
+            </div>
+            
+            <p>Don't have an account? Sign up</p>
+        </form>
     )
 }
 Header.defaultProps={
